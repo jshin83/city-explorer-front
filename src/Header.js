78a1036876e3b4e5459435a714130f981327607b
jshin83 <jen.kyongah.lee@gmail.com>
2019-05-08 10:27:16 -0700

    completed assignement

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Header extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1>City Explorer</h1>
+        <p>Enter a location below to learn about the weather, events, restaurants, movies filmed there, and more!</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
