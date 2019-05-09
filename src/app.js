1580780196245c5a53ccb0e4b8ef658ef28a0c42
jshin83 <jen.kyongah.lee@gmail.com>
2019-05-09 09:36:16 -0700

    add in app.js

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './Header.js';
+import Main from './Main.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Main />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
