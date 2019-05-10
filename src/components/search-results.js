5d956e024935e9ca093fa2b2572d3c046deece36
jshin83 <jen.kyongah.lee@gmail.com>
2019-05-09 23:08:35 -0700

    all lists showing

--- src/components/search-results.js
+++ src/components/search-results.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import superagent from 'superagent';
 class SearchResults extends React.Component {
 
   render() {
@@ -16,141 +15,105 @@ class SearchResults extends React.Component {
 }
 
 class Event extends React.Component {
-// `https://www.eventbriteapi.com/v3/events/search?location.longitude=${request.query.data.longitude}&location.latitude=${request.query.data.latitude}&expand=venue&token=${process.env.EVENTBRITE_API_KEY}`
-    // let data = superagent.get('https://calm-taiga-81023.herokuapp.com/events')
-    //   .query( { data: `${this.props.location}` })
-    //   .then( results => {
-    //     console.log('in EVENTS', results.body);
-    //   });
-      //   return results.body.reduce((list, event) => {
-      //     list[event.name] = event.url;
-      //     return list;
-      //   }, {});
-      // }).then( list => this.setState( {list}));
-
-      // render() {
-      //   const allWeather = this.props.darkSky.map((forecast, i) => {
-      //     return (
-      //         <
-      //         key={i}
-      //         forecast={forecast}
-      //         />
-      //     )
-      //   })
-      
-
-    render() {
-        console.log('event component', this.props.events);
-    return (
-      <ul>
-        {Object.keys(this.props.events).map((key, idx, array) => {
-          return (
-            <li key={idx}>
-              <a href={`${array[idx]['link']}`}>{key}</a>
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
-}
-
-class MovieDB extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     movies: this.props.movies
-  //   }
-  // };
-
-  //   let data = await superagent.get('https://calm-taiga-81023.herokuapp.com/movies');
-  //   let movies = data.body.results.reduce((list, movie) => {
-  //     list[movie.name] = movie.url;
-  //     return list;
-  //   }, {});
-  // }
   render() {
-    let allMovies = [];
-    if(this.props.movies) {
-      allMovies = this.props.movies.map((movie, i) => {
+    let allEvents = [];
+    if(this.props.events) {
+      allEvents = this.props.events.map((event, i) => {
         return (
-          <li key={i}>{movie.title}</li>
+          <ul key={i}>
+            <li>{event.name}, {event.event_date}</li>
+            <li><a href={event.link}>{event.name}</a></li>
+            <li>{event.summary}</li>
+          </ul>
         )
       })
     }
     return (
-      <ul>
+      <div>
+        <h2>Events</h2>
+        {allEvents}
+      </div>
+    )
+  }
+}
+
+class MovieDB extends React.Component {
+  render() {
+    let allMovies;
+    if(this.props.movies) {
+      allMovies = this.props.movies.map((movie, i) => {
+        return (
+          <ul key={i}>
+            <li><img src={movie.image_url} alt={movie.title}/></li>
+            <li>Movie Title: {movie.title}</li>
+            <li>{movie.overview}</li>
+          </ul>
+        )
+      })
+    }
+
+    return (
+      <div>
+        <h2>Movies</h2>
         {allMovies}
-      </ul>
+      </div>
     )
   }
 }
 
 class Weather extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     results: {}
-  //   }
-
-  //   let data = await superagent.get('https://calm-taiga-81023.herokuapp.com/weather');
-  //   let weathers = data.body.results.reduce((list, weather) => {
-  //     list[weather.name] = weather.url;
-  //     return list;
-  //   }, {});
-  // }
   render() {
+    let allWeather = [];
+    if(this.props.weather) {
+      allWeather = this.props.weather.map((day, i) => {
+        return (
+          <ul key={i}>
+            <li>{day.time}</li>
+            <li>{day.forecast}</li>
+          </ul>
+        )
+      })
+    }
     return (
-      <section>
-        <ul>Hi.</ul>
-      </section>
-    );
+      <div>
+        <h2>Weather</h2>
+        {allWeather}
+      </div>
+    )
   }
 }
 
 class Trail extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-
-  //   }
-
-  //   let data = await superagent.get('https://calm-taiga-81023.herokuapp.com/trail');
-  //   let trails = data.body.results.reduce((list, trail) => {
-  //     list[trail.name] = trail.url;
-  //     return list;
-  //   }, {});
-  // }
-
   render() {
     return (
-      <section>
-        <ul>Hi.</ul>
-      </section>
+      <div>
+        <h2>Trails</h2>
+      </div>
     );
   }
 }
 
 class Yelp extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-
-  //   }
-
-  //   let data = await superagent.get('https://calm-taiga-81023.herokuapp.com/yelp');
-  //   let yelps = data.body.results.reduce((list, yelp) => {
-  //     list[yelp.name] = yelp.url;
-  //     return list;
-  //   }, {});
-  // }
-
   render() {
+    let allYelp = [];
+    if(this.props.yelp) {
+      allYelp = this.props.yelp.map((business, i) => {
+        return (
+          <ul key={i}>
+            <li>{business.name}, {business.price}</li>
+            <li>Rating: {business.rating}</li>
+            <li><img src={business.image_url} alt={business.name} /></li>
+          </ul>
+        )
+      })
+    }
+
     return (
-      <section>
-        <ul>Hi.</ul>
-      </section>
-    );
+      <div>
+        <h2>Yelp</h2>
+        {allYelp}
+      </div>
+    )
   }
 }
 
