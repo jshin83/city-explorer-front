022c1ab99a7bcf7fa5513253aa209ac247a42210
jshin83 <jen.kyongah.lee@gmail.com>
2019-05-09 19:34:25 -0700

    almost completed api calls

--- src/components/app.js
+++ src/components/app.js
@@ -3,13 +3,19 @@ import Header from './header.js';
 import SearchResults from './search-results.js';
 import SearchForm from './search-form.js';
 import Map from './map.js';
+import superagent from 'superagent';
+const SERVER = 'https://calm-taiga-81023.herokuapp.com';
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      location: {}, 
+      location: "", 
       latitude: 47.6062095, 
-      longitude: -122.3320708
+      longitude: -122.3320708,
+      weather: [],
+      events: [],
+      movies: [],
+      yelp: []
     };
   }
 
@@ -18,6 +24,28 @@ class App extends React.Component {
     console.log('in app', result);
     this.setState({latitude: result.latitude});
     this.setState({longitude: result.longitude})
+    this.handleAPICalls();
+  }
+
+  handleAPICalls = () => {
+    this.getResource('weather');
+    this.getResource('movies');
+    this.getResource('yelp');
+    this.getResource('trails');
+    this.getResource('events');
+  }
+
+  getResource = async (resource) => {
+    console.log('in get event api call ', this.state.location);
+    await superagent.get(`${SERVER}/${resource}`)
+    .query( { data: { location: this.state.location, latitude: this.state.latitude, longitude: this.state.longitude} })
+    .then( results => {
+      console.log('in EVENTS', results);
+    });
+  }
+
+  getWeather = async () => {
+    await superagent.get()
   }
 
   render() {
@@ -26,7 +54,7 @@ class App extends React.Component {
         <Header titlePrompt="City Explorer" headerPrompt="Enter a location below to learn about the weather, events, restaurants, movies filmed there, and more!"/>
         <SearchForm handleLocation={this.handleLocation} prompt="Search for a location" buttonPrompt="Explore!" />
         <Map mapQuery={`https://maps.googleapis.com/maps/api/staticmap?center=${this.state.latitude}%2c%20${this.state.longitude}&zoom=13&size=600x300&maptype=roadmap&key=${process.env.REACT_APP_GEOCODE_API_KEY}`} mapAlt={this.state.location} />
-        <SearchResults lat={this.state.latitude} long={this.state.longitude} />
+        <SearchResults location= {this.state.location} lat={this.state.latitude} long={this.state.longitude} />
       </React.Fragment>
     );
   }
--- src/components/search-results.js
+++ src/components/search-results.js
@@ -1,22 +1,45 @@
 import React from 'react';
+import superagent from 'superagent';
 class SearchResults extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
 
   render() {
     return (
       <section>
         <Weather lat ={this.props.lat} long={this.props.long} />
         <Yelp lat={this.props.lat} long={this.props.long} />
-        <Event lat={this.props.lat} long={this.props.long} />
+        <Event location={this.props.location} />
         <MovieDB lat={this.props.lat} long={this.props.long} />
         <Trail lat={this.props.lat}long={this.props.long} />
       </section>
     );
   }
 }
+
 class Event extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: {}
+    }
+// `https://www.eventbriteapi.com/v3/events/search?location.longitude=${request.query.data.longitude}&location.latitude=${request.query.data.latitude}&expand=venue&token=${process.env.EVENTBRITE_API_KEY}`
+    // let data = superagent.get('https://calm-taiga-81023.herokuapp.com/events')
+    //   .query( { data: `${this.props.location}` })
+    //   .then( results => {
+    //     console.log('in EVENTS', results.body);
+    //   });
+      //   return results.body.reduce((list, event) => {
+      //     list[event.name] = event.url;
+      //     return list;
+      //   }, {});
+      // }).then( list => this.setState( {list}));
+
+  }
 
   render() {
     return (
@@ -26,8 +49,20 @@ class Event extends React.Component {
     );
   }
 }
-class MovieDB extends React.Component {
 
+class MovieDB extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+
+  //   }
+
+  //   let data = await superagent.get('https://calm-taiga-81023.herokuapp.com/movies');
+  //   let movies = data.body.results.reduce((list, movie) => {
+  //     list[movie.name] = movie.url;
+  //     return list;
+  //   }, {});
+  // }
   render() {
     return (
       <section>
@@ -38,7 +73,18 @@ class MovieDB extends React.Component {
 }
 
 class Weather extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     results: {}
+  //   }
 
+  //   let data = await superagent.get('https://calm-taiga-81023.herokuapp.com/weather');
+  //   let weathers = data.body.results.reduce((list, weather) => {
+  //     list[weather.name] = weather.url;
+  //     return list;
+  //   }, {});
+  // }
   render() {
     return (
       <section>
@@ -49,6 +95,18 @@ class Weather extends React.Component {
 }
 
 class Trail extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+
+  //   }
+
+  //   let data = await superagent.get('https://calm-taiga-81023.herokuapp.com/trail');
+  //   let trails = data.body.results.reduce((list, trail) => {
+  //     list[trail.name] = trail.url;
+  //     return list;
+  //   }, {});
+  // }
 
   render() {
     return (
@@ -58,7 +116,20 @@ class Trail extends React.Component {
     );
   }
 }
+
 class Yelp extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+
+  //   }
+
+  //   let data = await superagent.get('https://calm-taiga-81023.herokuapp.com/yelp');
+  //   let yelps = data.body.results.reduce((list, yelp) => {
+  //     list[yelp.name] = yelp.url;
+  //     return list;
+  //   }, {});
+  // }
 
   render() {
     return (
